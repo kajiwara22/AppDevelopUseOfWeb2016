f203a914816dcde78b9d619f88b6693b98f4379a
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-03 06:18:01 +0900

    flywayの導入と投入用SQLの配置

---
+++ SecondSprintAPP/src/main/resources/db/migration/V1__Create_initial_tables.sql
@@ -0,0 +1,8 @@
+alter database u20511091 default character set utf8;
+drop table if exists user cascade;
+create table user(
+	user_id VARCHAR(16) not null
+    , user_name VARCHAR(128) not null
+    ,primary key (user_id)
+);
+insert into user(user_id,user_name) value ('20511091yk','かじわらゆたか');
