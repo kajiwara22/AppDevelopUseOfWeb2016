5840900a4ad315bff77f1c5b047817e2eca97e8c
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-02 10:36:55 +0900

    プロジェクトの雛形をコミット

---
+++ SecondSprintAPP/src/main/java/jp/ac/tama/SecondSpringAppApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.tama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringAppApplication.class, args);
+	}
+}
