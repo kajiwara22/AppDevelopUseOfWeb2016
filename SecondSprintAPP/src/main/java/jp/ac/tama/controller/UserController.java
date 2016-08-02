f2a27cb7c779cc2857294f029b5dc283120ab71b
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-03 08:38:56 +0900

    ユーザー一覧習得を実装

--- SecondSprintAPP/src/main/java/jp/ac/tama/controller/UserController.java
+++ SecondSprintAPP/src/main/java/jp/ac/tama/controller/UserController.java
@@ -1,13 +1,15 @@
 package jp.ac.tama.controller;
 
+import jp.ac.tama.entity.UserEntity;
+import jp.ac.tama.model.User;
+import jp.ac.tama.service.UserService;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import jp.ac.tama.model.User;
-import lombok.val;
 
 /**
  * Created by kajiwarayutaka on 2016/08/03.
@@ -15,9 +17,17 @@ import lombok.val;
 @RestController
 @RequestMapping(value = "/user", produces = { MediaType.APPLICATION_JSON_VALUE })
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "{userId}",method = RequestMethod.GET)
     public User showUser(@PathVariable String userId){
         val user = new User(userId,"リクエストより生成されたユーザー");
         return user;
+    }
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public Iterable<UserEntity> showUsers(){
+        val users = userService.getUsers();
+        return users;
     }
 }
