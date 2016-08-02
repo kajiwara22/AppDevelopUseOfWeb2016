7c4273d62b15195deafdca99f77d9c052d6948ad
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-03 04:28:27 +0900

    Userクラスを作成

---
+++ SecondSprintAPP/src/main/java/jp/ac/tama/model/User.java
@@ -0,0 +1,14 @@
+package jp.ac.tama.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by kajiwarayutaka on 2016/08/03.
+ */
+@Data
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String name;
+}
