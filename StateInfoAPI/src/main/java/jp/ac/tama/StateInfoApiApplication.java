ca670b346e7ae0bdc265ad3c5cb7184554b243d9
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-04 15:17:14 +0900

    プロジェクトの雛形を作成

---
+++ StateInfoAPI/src/main/java/jp/ac/tama/StateInfoApiApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.tama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateInfoApiApplication.class, args);
+	}
+}
