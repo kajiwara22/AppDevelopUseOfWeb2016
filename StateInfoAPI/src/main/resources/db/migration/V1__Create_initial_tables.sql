22fc190bb3d3129814f978e54fff30a3a2ebf757
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2016-08-04 15:34:32 +0900

    flywayでのDB Migrationを実施

---
+++ StateInfoAPI/src/main/resources/db/migration/V1__Create_initial_tables.sql
@@ -0,0 +1,53 @@
+create table stateInfo(
+`StateId` int NOT NULL PRIMARY KEY,
+`name` varchar(255) DEFAULT NULL,
+`lat` double(9,6),
+`lng` double(9,6)
+);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (1	,'北海道'	,43.06417,	141.34694);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (2	,'青森県'	,40.82444,	140.74);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (3	,'岩手県'	,39.70361,	141.1525);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (4	,'宮城県'	,38.26889,	140.87194);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (5	,'秋田県'	,39.71861,	140.1025);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (6	,'山形県'	,38.24056,	140.36333);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (7	,'福島県'	,37.75,	140.46778);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (8	,'茨城県'	,36.34139,	140.44667);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (9	,'栃木県'	,36.56583,	139.88361);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (10	,'群馬県'	,36.39111,	139.06083);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (11	,'埼玉県'	,35.85694,	139.64889);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (12	,'千葉県'	,35.60472,	140.12333);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (13	,'東京都'	,35.68944,	139.69167);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (14	,'神奈川県'	,35.44778,	139.6425);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (15	,'新潟県'	,37.90222,	139.02361);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (16	,'富山県'	,36.69528,	137.21139);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (17	,'石川県'	,36.59444,	136.62556);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (18	,'福井県'	,36.06528,	136.22194);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (19	,'山梨県'	,35.66389,	138.56833);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (20	,'長野県'	,36.65139,	138.18111);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (21	,'岐阜県'	,35.39111,	136.72222);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (22	,'静岡県'	,34.97694,	138.38306);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (23	,'愛知県'	,35.18028,	136.90667);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (24	,'三重県'	,34.73028,	136.50861);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (25	,'滋賀県'	,35.00444,	135.86833);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (26	,'京都府'	,35.02139,	135.75556);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (27	,'大阪府'	,34.68639,	135.52);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (28	,'兵庫県'	,34.69139,	135.18306);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (29	,'奈良県'	,34.68528,	135.83278);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (30	,'和歌山県'	,34.22611,	135.1675);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (31	,'鳥取県'	,35.50361,	134.23833);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (32	,'島根県'	,35.47222,	133.05056);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (33	,'岡山県'	,34.66167,	133.935);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (34	,'広島県'	,34.39639,	132.45944);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (35	,'山口県'	,34.18583,	131.47139);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (36	,'徳島県'	,34.06583,	134.55944);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (37	,'香川県'	,34.34028,	134.04333);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (38	,'愛媛県'	,33.84167,	132.76611);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (39	,'高知県'	,33.55972,	133.53111);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (40	,'福岡県'	,33.60639,	130.41806);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (41	,'佐賀県'	,33.24944,	130.29889);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (42	,'長崎県'	,32.74472,	129.87361);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (43	,'熊本県'	,32.78972,	130.74167);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (44	,'大分県'	,33.23806,	131.6125);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (45	,'宮崎県'	,31.91111,	131.42389);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (46	,'鹿児島県'	,31.56028,	130.55806);
+insert into stateInfo(`StateId`,`name`,`lat`,`lng`) values (47	,'沖縄県'	,26.2125,	127.68111);
